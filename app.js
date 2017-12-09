6039f58b6feed972fcb10ef2f31a97414ec9b4cd
Bryce Emery <bemery005@student.butte.edu>
2017-12-09 02:04:35 +0000

    started putting down routing to code. added a number of placeholding ejs files.

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index  = require('./routes/index');
+var users  = require('./routes/users');
+// UNCOMMENT A COMPLETED
+// var editer = require('./routes/editer');
+// var print  = require('./routes/print');
+// var search = require('./search');
 
 var app = express();
 
@@ -24,6 +28,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+// UNCOMMENT A COMPLETED
+// app.use('/puzzle-maker', editer);
+// app.use('/printout', print);
+// app.use('/puzzles', search);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
